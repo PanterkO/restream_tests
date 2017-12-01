2cb6e341b83df657fe549cbe85cf5f5050c3d325
ADanilina <ladyintegral666@yandex.ru>
2017-12-01 18:25:44 +0300

    2

--- test_owasp.py
+++ test_owasp.py
@@ -17,69 +17,17 @@ from wdriver import Driver
 @pytest.fixture
 def driver(request):
     fixture = Driver()
-    request.addfinalizer(fixture.browser_close())
+    #request.addfinalizer(fixture.browser_close())
     return fixture
 
 def test_login_in_owasp(driver):
 
-    open_url('https://restream.sloppy.zone')
-    login('restream5@mailinator.com','123456')
+    driver.open_url('https://restream.sloppy.zone')
+    driver.login('restream5@mailinator.com','123456')
 
     time.sleep(.5)
-    search_result=search_product()
-    assert_result(search_result)
-    add_the_cheapest_product_in_basket(search_result)
-    check_products_in_basket()
+    search_result = driver.search_product('OWASP')
+    driver.assert_result(search_result)
+    driver.add_the_cheapest_product_in_basket(search_result)
+    driver.check_products_in_basket()
     time.sleep(10)
-
-
-def check_products_in_basket():
-    driver.find_element_by_xpath('//nav//a[@href="#/basket"]').click()
-    basket = driver.find_elements_by_xpath('//tbody/tr[@class="ng-scope"]')
-    for product in basket:
-        product_name = product.find_element_by_xpath('.td[1]')
-        product_price = product.find_element_by_xpath('.td[3]')
-
-
-def add_the_cheapest_product_in_basket(search_result):
-    max_price = 0
-    cheapest_wear = None
-    for result in search_result:
-        price = result.find_element_by_xpath('./td[4]')
-        if float(price.text) > max_price:
-            max_price = float(price.text)
-            cheapest_wear = result
-    cheapest_wear.find_element_by_xpath('./td[5]//i[contains(@class, "fa-cart-plus")]').click()
-    max_price = 0
-    for result in search_result:
-        price = result.find_element_by_xpath('./td[4]')
-        if float(price.text) > max_price and result != cheapest_wear:
-            max_price = float(price.text)
-            cheapest_wear = result
-    cheapest_wear.find_element_by_xpath('./td[5]//i[contains(@class, "fa-cart-plus")]').click()
-
-
-def assert_result(search_result):
-    time.sleep(.5)
-
-    assert len(search_result) == 10
-    return search_result
-
-
-def search_product(product='OWASP'):
-    driver.find_element_by_xpath('//nav//input[@ng-model="searchQuery"]').send_keys(owasp)
-    driver.find_element_by_xpath('//*[@id="searchButton"]').click()
-    search_result = driver.find_elements_by_xpath('//tbody/tr[@class="ng-scope"]')
-    return search_result
-
-
-def login(login, password):
-    driver.find_element_by_xpath('//a[@href="#/login"]').click()
-    time.sleep(.5)
-    driver.find_element_by_xpath('//*[@id="userEmail"]').send_keys(login)
-    driver.find_element_by_xpath('//*[@id="userPassword"]').send_keys(password)
-    driver.find_element_by_xpath('//*[@id="loginButton"]').click()
-
-
-def open_url(url):
-    driver.get(url)
--- wdriver.py
+++ wdriver.py
@@ -1,11 +1,72 @@
 from selenium.webdriver.chrome.webdriver import WebDriver
+import time
 
 class Driver:
 
     def __init__(self):
         self.wd = WebDriver()
+        self.wd.maximize_window()
         self.wd.implicitly_wait(60)
+       # self.wd.capabilities()
+
 
     def browser_close(self):
         self.wd.close()
         self.wd.quit()
+
+
+    def check_products_in_basket(self):
+        self.wd.find_element_by_xpath('//nav//a[@href="#/basket"]').click()
+        basket = self.wd.find_elements_by_xpath('//tbody/tr[@class="ng-scope"]')
+        for product in basket:
+            product_name = product.find_element_by_xpath('.//td[1]')
+            product_price = product.find_element_by_xpath('.//td[3]')
+
+
+    def add_the_cheapest_product_in_basket(self, search_result):
+        max_price = 0
+        cheapest_wear = None
+        for result in search_result:
+            price = result.find_element_by_xpath('./td[4]')
+            if float(price.text) > max_price:
+                max_price = float(price.text)
+                cheapest_wear = result
+        cheapest_wear.find_element_by_xpath('./td[5]//i[contains(@class, "fa-cart-plus")]').click()
+        max_price = 0
+        for result in search_result:
+            price = result.find_element_by_xpath('./td[4]')
+            if float(price.text) > max_price and result != cheapest_wear:
+                max_price = float(price.text)
+                cheapest_wear2 = result
+        cheapest_wear2.find_element_by_xpath('./td[5]//i[contains(@class, "fa-cart-plus")]').click()
+
+
+    def assert_result(self, search_result):
+        time.sleep(.5)
+
+        assert len(search_result) == 10
+
+
+    def search_product(self, product):
+        self.wd.find_element_by_xpath('//nav//input[@ng-model="searchQuery"]').send_keys(product)
+        self.wd.find_element_by_xpath('//*[@id="searchButton"]').click()
+        js_script = "var performance = window.performance || window.mozPerformance || window.msPerformance || window.webkitPerformance || {}; var network = performance.getEntries() || {}; return network;"
+        js_result = self.wd.execute_script(js_script)
+        print(js_result)
+        #print(self.wd.get_log('browser'))
+        search_result = self.wd.find_elements_by_xpath('//tbody/tr[@class="ng-scope"]')
+        return search_result
+
+
+    def login(self, login, password):
+        self.wd.find_element_by_xpath('//a[@href="#/login"]').click()
+        time.sleep(.5)
+        self.wd.find_element_by_xpath('//*[@id="userEmail"]').send_keys(login)
+        self.wd.find_element_by_xpath('//*[@id="userPassword"]').send_keys(password)
+        self.wd.find_element_by_xpath('//*[@id="loginButton"]').click()
+
+
+    def open_url(self, url):
+        time.sleep(5)
+        wd = self.wd
+        wd.get(url)
